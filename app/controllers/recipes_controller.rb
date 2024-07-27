fdac3e0c331ebc1b6193e8f93bca959115379ba3
Ricardo Juarez <146899774+jrzricardo@users.noreply.github.com>
2024-07-27 23:11:04 +0000

    updated recipe routes and controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,8 +3,16 @@ class RecipesController < ApplicationController
   end
 
   def create
+    scraped_data = RecipeScraper.scrape(params[:recipe][:url])
+    @recipe = Recipe.new(scraped_data)
+    if @recipe.save
+      redirect_to @recipe, notice: 'Recipe has been ripped!'
+    else
+      render:new
+    end
   end
 
   def show
+    @recipe = Recipe.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'recipes/new'
-  get 'recipes/create'
-  get 'recipes/show'
-
-  # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
-
-  # get "/your_first_screen" => "pages#first"
-  
+  resources :recipes, only: [:new, :create, :show]
+  root 'recipes#new'
 end
