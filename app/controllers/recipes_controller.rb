885e124cc25d438b1e86ab8930981d18de71bf10
Ricardo Juarez <146899774+jrzricardo@users.noreply.github.com>
2024-07-30 17:27:52 +0000

    Updated recipe controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,10 +1,62 @@
 class RecipesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_recipe, only: [:show, :edit, :update, :destroy]
+
   def index
+    @recipes = Recipe.all
+  end
+
+  def show
+  end
+
+  def new
+    @recipe = current_user.recipes.build
+  end
+
+  def create
+    @recipe = current_user.recipes.build(recipe_params)
+    if @recipe.save
+      redirect_to @recipe, notice: 'Recipe was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @recipe.update(recipe_params)
+      redirect_to @recipe, notice: 'Recipe was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @recipe.destroy
+    redirect_to recipes_url, notice: 'Recipe was successfully destroyed.'
   end
 
   def extract
     url = params[:url]
-    @recipe = Recipe.extract_from_url(url)
-    render :result
+    extracted_recipe = Recipe.extract_from_url(url)
+    @recipe = current_user.recipes.build(
+      name: extracted_recipe.name,
+      author: extracted_recipe.author,
+      ingredients: extracted_recipe.ingredients,
+      instructions: extracted_recipe.instructions
+    )
+    render :new
+  end
+
+  private
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :author, ingredients: [], instructions: [])
   end
 end
