81be68af859b12dbf018af312243182fca5ab5f6
Ricardo Juarez <146899774+jrzricardo@users.noreply.github.com>
2024-08-02 15:25:05 +0000

    Added log out function

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'landing#index'
+  
   get 'home', to: 'home#index'
-
   get 'rip_recipe', to: 'recipes#rip'
   # get "/recipes/:name" => "recipes/rip", as: :recipes_rip
-
-  
 
   resources :recipes, only: [:index, :show] do
     post 'save', on: :member
