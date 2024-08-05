40a1f291e59998f601998b2d2bc3bb35b05579c1
Ricardo Juarez <146899774+jrzricardo@users.noreply.github.com>
2024-08-05 14:51:41 +0000

    Updated render.yaml and render-build

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,11 +1,12 @@
 # Exit immediately if a command exits with a non-zero status.
-set -e
+set -o errexit
 
 # Install dependencies
 bundle install
 
 # Precompile assets
 bundle exec rake assets:precompile
-bundle exec rails db:migrate
+bundle exec rake assets:clean
+bundle exec rake db:migrate
 
 echo "Build successful!"
